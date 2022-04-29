f2187188f605337ce6dd5cea3642446725ce19ce
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-04-29 21:28:52 +0100

    Create CollatzSequence.php

---
+++ CollatzSequence.php
@@ -0,0 +1,16 @@
+<?php
+function printCollatz($n)
+{
+    while ($n != 1)
+    {
+        echo $n . " ";
+        if ($n & 1)
+            $n = 3 * $n + 1;
+            else
+            $n = $n / 2;
+    }
+    echo $n;
+}
+//Enter number you wish to input here.
+printCollatz(40);
+?>
